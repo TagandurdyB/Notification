fec20f98562177f13329c65a88f053962a316fb6
TagandurdyB <TBMtkm2003@gmail.com>
2023-02-20 18:33:55 +0500

    0.0.1

---
+++ ViewModel/Providers/providers.dart
@@ -0,0 +1,15 @@
+import '/View/Scaffold/provider_app_bar.dart';
+import '/ViewModel/Providers/provider_theme.dart';
+import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
+
+class MyProvoders {
+  static List<SingleChildWidget> get list => [
+    ChangeNotifierProvider<ProviderTheme>(
+      create:(context)=> ProviderTheme(),
+    ),
+    ChangeNotifierProvider<ProviderAppBar>(
+      create:(context)=> ProviderAppBar(),
+    ),
+  ];
+}
---
+++ ViewModel/routes_vm.dart
@@ -0,0 +1,15 @@
+import '/View/Pages/page_home.dart';
+import 'package:flutter/material.dart';
+
+import '../View/Pages/page_logo.dart';
+
+class Rout {
+  static const String logo = "/LogoPage";
+    static const String home = "/HomePage";
+
+
+  static Map<String, Widget Function(dynamic)> pages = {
+    logo: (context) => const LogoPage(),
+    home: (context) => const HomePage(),
+  };
+}
